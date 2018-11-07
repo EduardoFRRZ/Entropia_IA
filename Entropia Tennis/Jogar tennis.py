3b907c508548a79a468a44108f6915b96bb305c1
Eduardo Ferrarezi <eduardogd20@gmail.com>
2018-11-07 15:34:09 -0200

    Commit entropia

--- Entropia Tennis/Jogar tennis.py
+++ Entropia Tennis/Jogar tennis.py
@@ -54,14 +54,20 @@ print("TEMPERATURA = %.3f\n" %(ganhoTemperatura))
 #################################################################################################################
 #################################################################################################################
 
-entropiaEnsolaradoQuente = - (2/2) * log2(2/2)
-print("Entropia ensolarado quente = %0.3f\n" %(entropiaEnsolaradoQuente))
+'''ENSOLARADO'''
 
-entropiaEnsolaradoModerado = - (1/2) * log2(1/2) - (1/2) * log2(1/2)
-print("Entropia ensolarado moderado = %0.3f\n" %(entropiaEnsolaradoModerado))
 
-entropiaEnsolaradoFresca = - (1/1) * log2(1/1)
-print("Entropia ensolarado fresca = %0.3f\n" %(entropiaEnsolaradoFresca))
+entropiaEnsolaradoTemperaturaQuente = - (2/2) * log2(2/2)
+print("Entropia ensolarado quente = %0.3f\n" %(entropiaEnsolaradoTemperaturaQuente))
+
+entropiaEnsolaradoTemperaturaModerado = - (1/2) * log2(1/2) - (1/2) * log2(1/2)
+print("Entropia ensolarado moderado = %0.3f\n" %(entropiaEnsolaradoTemperaturaModerado))
+
+entropiaEnsolaradoTemperaturaFresca = - (1/1) * log2(1/1)
+print("Entropia ensolarado fresca = %0.3f\n" %(entropiaEnsolaradoTemperaturaFresca))
+
+ganhoEnsolaradoTemperatura = 0.971 - ((2/5 * entropiaEnsolaradoTemperaturaQuente) + (2/5 * entropiaEnsolaradoTemperaturaModerado) + (1/5 * entropiaEnsolaradoTemperaturaFresca))
+print("GANHO ENSOLARADO TEMPERATURA = %.3f\n" %(ganhoEnsolaradoTemperatura))
 
 #################################################################################################################
 
@@ -71,6 +77,10 @@ print("Entropia ensolarado humidade alta = %0.3f\n" %(entropiaEnsolaradoHumidade
 entropiaEnsolaradoHumidadeNormal = - (2/2) * log2(2/2)
 print("Entropia ensolarado humidade normal = %0.3f\n" %(entropiaEnsolaradoHumidadeNormal))
 
+ganhoEnsolaradoHumidade = 0.971 - ((3/5 * entropiaEnsolaradoHumidadeAlta) + (2/5 * entropiaEnsolaradoHumidadeNormal))
+print("GANHO ENSOLARADO HUMIDADE = %.3f\n" %(ganhoEnsolaradoHumidade))
+
+
 #################################################################################################################
 
 entropiaEnsolaradoVentoForte = - (1/2) * log2(1/2) - (1/2) * log2(1/2)
@@ -78,3 +88,82 @@ print("Entropia ensolarado vento forte = %0.3f\n" %(entropiaEnsolaradoVentoForte
 
 entropiaEnsolaradoVentoFraco = - (1/3) * log2(1/3) - (2/3) * log2(2/3)
 print("Entropia ensolarado vento fraco = %0.3f\n" %(entropiaEnsolaradoVentoFraco))
+
+ganhoEnsolaradoVento = 0.971 - ((2/5 * entropiaEnsolaradoVentoForte) + (3/5 * entropiaEnsolaradoVentoFraco))
+print("GANHO ENSOLARADO VENTO = %.3f\n" %(ganhoEnsolaradoVento))
+
+##################################################################################################################
+''''CHUVOSO'''
+
+entropiaChuvosoTemperaturaModerado = - (2/3) * log2(2/3) - (1/3) * log2(1/3)
+print("Entropia Chuvoso moderado = %0.3f\n" %(entropiaChuvosoTemperaturaModerado))
+
+entropiaChuvosoTemperaturaFresca = - (1/2) * log2(1/2) - (1/2) * log2(1/2)
+print("Entropia Chuvoso fresca = %0.3f\n" %(entropiaChuvosoTemperaturaFresca))
+
+ganhoChuvosoTemperatura = 0.971 - ((2/5 * entropiaChuvosoTemperaturaModerado) + (1/5 * entropiaEnsolaradoTemperaturaFresca))
+print("GANHO CHUVOSO TEMPERATURA = %.3f\n" %(ganhoChuvosoTemperatura))
+
+
+################################################################################
+
+entropiaChuvosoHumidadeAlta = - (2/2) * log2(2/2)
+print("Entropia Chuvoso humidade alta = %0.3f\n" %(entropiaChuvosoHumidadeAlta))
+
+entropiaChuvosoHumidadeNormal = - (2/3) * log2(2/3) - (1/3) * log2(1/3)
+print("Entropia Chuvoso humidade normal = %0.3f\n" %(entropiaChuvosoHumidadeNormal))
+
+ganhoChuvosoHumidade = 0.971 - ((2/5 * entropiaChuvosoHumidadeAlta) + (3/5 * entropiaChuvosoHumidadeNormal))
+print("GANHO CHUVOSO HUMIDADE = %.3f\n" %(ganhoChuvosoHumidade))
+
+##################################################################################################################
+
+
+entropiaChuvosoVentoForte = - (2/2) * log2(2/2)
+print("Entropia Chuvoso vento forte = %0.3f\n" %(entropiaChuvosoVentoForte))
+
+entropiaChuvosoVentoFraco = - (3/3) * log2(3/3)
+print("Entropia Chuvoso vento fraco = %0.3f\n" %(entropiaChuvosoVentoFraco))
+
+ganhoChuvosoVento = 0.971 - ((2/5 * entropiaChuvosoVentoForte) + (3/5 * entropiaChuvosoVentoFraco))
+print("GANHO CHUVOSO VENTO = %.3f\n" %(ganhoChuvosoVento))
+
+
+##################################################################################################################
+'''NUBLADO'''
+
+entropiaNubladoTemperaturaQuente = - (2/2) * log2(2/2)
+print("Entropia Nublado quente = %0.3f\n" %(entropiaNubladoTemperaturaQuente))
+
+entropiaNubladoTemperaturaModerado = - (1/1) * log2(1/1)
+print("Entropia Nublado moderado = %0.3f\n" %(entropiaNubladoTemperaturaModerado))
+
+entropiaNubladoTemperaturaFresca = - (1/1) * log2(1/1)
+print("Entropia Nublado fresca = %0.3f\n" %(entropiaNubladoTemperaturaFresca))
+
+ganhoNubladoTemperatura = 0.0 - ((2/5 * entropiaNubladoTemperaturaQuente) + (2/5 * entropiaNubladoTemperaturaModerado) + (1/5 * entropiaNubladoTemperaturaFresca))
+print("GANHO NUBLADO TEMPERATURA = %.3f\n" %(ganhoNubladoTemperatura))
+
+##############################################################################33
+
+entropiaNubladoHumidadeAlta = - (2/2) * log2(2/2)
+print("Entropia Nublado humidade alta = %0.3f\n" %(entropiaNubladoHumidadeAlta))
+
+entropiaNubladoHumidadeNormal = - (2/2) * log2(2/2)
+print("Entropia Nublado humidade normal = %0.3f\n" %(entropiaNubladoHumidadeNormal))
+
+ganhoNubladoHumidade = 0.0 - ((2/5 * entropiaNubladoHumidadeAlta) + (3/5 * entropiaNubladoHumidadeNormal))
+print("GANHO NUBLADO HUMIDADE = %.3f\n" %(ganhoNubladoHumidade))
+
+##################################################################################################################
+
+entropiaNubladoVentoForte = - (2/2) * log2(2/2)
+print("Entropia Nublado vento forte = %0.3f\n" %(entropiaNubladoVentoForte))
+
+entropiaNubladoVentoFraco = - (2/2) * log2(2/2)
+print("Entropia Nublado vento fraco = %0.3f\n" %(entropiaNubladoVentoFraco))
+
+ganhoNubladoVento = 0.0 - ((2/5 * entropiaNubladoVentoForte) + (3/5 * entropiaNubladoVentoFraco))
+print("GANHO NUBLADO VENTO = %.3f\n" %(ganhoNubladoVento))
+
+##################################################################################################################
